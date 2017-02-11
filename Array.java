5f8e3f75fb564e2498c3b1047e95f0a06a0c56d4
Dileema <dileematl@gmail.com>
2017-02-11 15:20:09 +0530

    Create three new java files and add them

---
+++ Array.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Dileema on 2/11/2017.
+ */
+public class Array {
+    public static void main(String args[]){
+        int b[]=new int[100];
+        for(int n=0;n<100;n++){
+            b[n]=n+1;
+            System.out.println(b[n]);
+        }
+    }
+
+}
