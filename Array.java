4169973c9f439df269ad96d5c775f7344b83be38
Dileema <dileematl@gmail.com>
2017-02-11 15:35:10 +0530

    New change

--- Array.java
+++ Array.java
@@ -3,8 +3,8 @@
  */
 public class Array {
     public static void main(String args[]){
-        int b[]=new int[100];
-        for(int n=0;n<100;n++){
+        int b[]=new int[50];
+        for(int n=0;n<50;n++){
             b[n]=n+1;
             System.out.println(b[n]);
         }
